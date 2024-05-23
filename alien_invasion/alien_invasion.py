47a494dd6356e1fb7dcd226913d0b153887202a8
hdqk <hectord@gmail.com>
2024-05-23 16:21:55 -0400

    created a Settings module to hold all/most of the game settings

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@
 
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
@@ -11,8 +12,10 @@ class AlienInvasion:
         """Initialize the game and create game resources."""
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         # Set the background color.
@@ -27,7 +30,7 @@ class AlienInvasion:
                     sys.exit()
 
             # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
