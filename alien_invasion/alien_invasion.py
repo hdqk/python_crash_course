ab55d8bd31edf0958906fa337a3594796cf32e28
hdqk <hectord@gmail.com>
2024-05-24 13:04:35 -0400

    removed unused background color from AlienInvasion class

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -19,9 +19,6 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
 
-        # Set the background color.
-        self.bg_color = (230, 230, 230)
-
     def run_game(self):
         """Start the main loop for the game."""
         while True:
