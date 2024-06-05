5a58c9e7e9804d485d8602d11b6aa9bd4963a5af
hdqk <hectord@gmail.com>
2024-06-05 15:32:44 -0400

    copied alien_invasion files from chapter_12, added alien.bmp to /images

---
+++ chapter_13/alien_invasion/settings.py
@@ -0,0 +1,21 @@
+# Hector Delgado 5/23/2024
+
+class Settings:
+    """A class to store all setings for Alien Invasion."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1280
+        self.screen_height = 720
+        self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.5
+
+        # Bullet settings
+        self.bullet_speed = 3.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullet_limit = 3
