38c73ea8d374efe714a9a7590ddde545f4c7c854
hdqk <hectord@gmail.com>
2024-06-06 17:09:48 -0400

    removed unused variables from settings.py

--- chapter_13/stars/settings.py
+++ chapter_13/stars/settings.py
@@ -1,7 +1,7 @@
-# Hector Delgado 5/23/2024
+# Hector Delgado 6/6/2024
 
 class Settings:
-    """A class to store all setings for Alien Invasion."""
+    """A class to store all setings for Star Grid."""
 
     def __init__(self):
         """Initialize the game's settings."""
@@ -9,13 +9,3 @@ class Settings:
         self.screen_width = 1280
         self.screen_height = 720
         self.bg_color = (230, 230, 230)
-
-        # Ship settings
-        self.ship_speed = 1.5
-
-        # Bullet settings
-        self.bullet_speed = 3.0
-        self.bullet_width = 3
-        self.bullet_height = 15
-        self.bullet_color = (60, 60, 60)
-        self.bullet_limit = 3
