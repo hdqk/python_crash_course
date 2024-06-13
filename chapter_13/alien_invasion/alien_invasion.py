5b17f9d2dbbfd91ba3a7daf333e936dfde685544
hdqk <hectord@gmail.com>
2024-06-13 14:20:39 -0400

    alien-ship collisions now reset the game

--- chapter_13/alien_invasion/alien_invasion.py
+++ chapter_13/alien_invasion/alien_invasion.py
@@ -2,10 +2,12 @@
 
 import sys
 import pygame
+from time import sleep
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
+from game_stats import GameStats
 
 
 class AlienInvasion:
@@ -19,6 +21,8 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        # Create an instance to store game statistics.
+        self.stats = GameStats(self)
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
         self.aliens = pygame.sprite.Group()
@@ -97,7 +101,7 @@ class AlienInvasion:
 
         # Look for alien-ship collisions.
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
-            print("Ship hit!")
+            self._ship_hit()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
@@ -136,6 +140,22 @@ class AlienInvasion:
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1  # reverses direction on next edge
+
+    def _ship_hit(self):
+        """Respond to the ship being hit by an alien."""
+        # Decrease ships_left.
+        self.stats.ships_left -= 1
+
+        # Get rid of any remaining bullets and aliens.
+        self.bullets.empty()
+        self.aliens.empty()
+
+        # Create a new fleet and center the ship.
+        self._create_fleet()
+        self.ship.center_ship()
+
+        # Short pause.
+        sleep(1.0)
 
     def _update_screen(self):
         """Update images on the screen and flip to the new screen."""
