d8cf67fe79026d73625a728132f66e0ddc9654e8
hdqk <hectord@gmail.com>
2024-06-28 16:05:43 -0400

    added solution for problem 16-5 "explore"

--- chapter_16/explore_sd_rainfall.py
+++ chapter_16/explore_sd_rainfall.py
@@ -8,15 +8,20 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# for index, column_header in enumerate(header_row):
-#     print(index, column_header)  # print what's in the header
+for index, column_header in enumerate(header_row):
+    if column_header == "NAME":
+        station = index
+    if column_header == "DATE":
+        date = index
+    if column_header == "PRCP":
+        rain_id = index
 
-# Extract dates and temperatures.
+# Extract dates and rainfall.
 dates, rainfall = [], []
 for row in reader:
-    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    current_date = datetime.strptime(row[date], '%Y-%m-%d')
     try:
-        rain = float(row[3])
+        rain = float(row[rain_id])
     except ValueError:
         print(f"Missing data for {current_date}")
     else:
@@ -29,7 +34,7 @@ fig, ax = plt.subplots()
 ax.scatter(dates, rainfall, color='blue', s=10)
 
 # Format plot.
-title = "Daily Rainfall, 2023\nSanto Domingo, Dominican Republic"
+title = f"Daily Rainfall, 2023\n{row[station]}"
 ax.set_title(title, fontsize=20)
 ax.set_xlabel("", fontsize=16)
 fig.autofmt_xdate()
--- chapter_16/explore_sd_temps.py
+++ chapter_16/explore_sd_temps.py
@@ -8,16 +8,23 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# for index, column_header in enumerate(header_row):
-#     print(index, column_header)  # print what's in the header
+for index, column_header in enumerate(header_row):
+    if column_header == "NAME":
+        station = index
+    if column_header == "DATE":
+        date = index
+    if column_header == "TMAX":
+        tmax = index
+    if column_header == "TMIN":
+        tmin = index
 
 # Extract dates and temperatures.
 dates, highs, lows = [], [], []
 for row in reader:
-    current_date = datetime.strptime(row[2], '%Y-%m-%d')
+    current_date = datetime.strptime(row[date], '%Y-%m-%d')
     try:
-        high = int(row[5])
-        low = int(row[6])
+        high = (((int(row[tmax])-32)*5)/9)  # convert F into C
+        low = (((int(row[tmin])-32)*5)/9)
     except ValueError:
         print(f"Missing data for {current_date}")
     else:
@@ -31,13 +38,14 @@ fig, ax = plt.subplots()
 ax.plot(dates, highs, color='red', alpha=0.5)
 ax.plot(dates, lows, color='blue', alpha=0.5)
 ax.fill_between(dates, highs, lows, facecolor='yellow', alpha=0.1)
+ax.set(ylim=(10, 45))
 
 # Format plot.
-title = "Daily Temperatures, 2023\nSanto Domingo, Dominican Republic"
+title = f"Daily Temperatures, 2023\n{row[station]}"
 ax.set_title(title, fontsize=20)
 ax.set_xlabel("", fontsize=16)
 fig.autofmt_xdate()
-ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.set_ylabel("Temperature (C)", fontsize=16)
 ax.tick_params(labelsize=16)
 
 plt.show()
