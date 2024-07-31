4242115df52f2f61c9d412b48804b819ee86c722
hdqk <hectord@gmail.com>
2024-07-31 13:50:01 -0400

    added all example exercises up to problem 20-3

--- chapter_20/learning_log/ll_project/settings.py
+++ chapter_20/learning_log/ll_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y0a%ks8z(qzbdgxbf(ygu+&f=#(z5^6ocwf9h#%f3pr#+_%rj0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'll_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
